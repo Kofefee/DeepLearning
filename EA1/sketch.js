3b65349bfed8c4683756e1eb6975c805c052d714
Felix <felix.koelle@hs-furtwangen.de>
2026-04-06 17:56:04 +0200

    update sketch.js

--- EA1/sketch.js
+++ EA1/sketch.js
@@ -5,39 +5,40 @@ let label = "";
 let confidence = "";
 
 function preload() {
-  classifier = ml5.imageClassifier("MobileNet");
-  img = loadImage("images/bird.png");
+    classifier = ml5.imageClassifier("MobileNet");
+    img = loadImage("images/bird.png");
 }
 
 function setup() {
-  createCanvas(400, 400);
-  classifier.classify(img, gotResult);
-  image(img, 300, 300);
+    createCanvas(400, 400);
+    classifier.classify(img, gotResult);
+    image(img, 300, 300);
 }
 
 function draw() {
-  background(220);
+    background(220);
+
+    myDiv = document.getElementById('myDiv');
+    console.log("This is my Div:" + myDiv);
+    Plotly.newPlot(myDiv, [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16]
+    }], {
+        margin: { t: 0 }
+    });
 }
 
 function gotResult(results) {
-  console.log(results);
-  
+    console.log(results);
+
     fill(255);
-  stroke(0);
-  textSize(18);
-  label = "Label: " + results[0].label;
-  confidence = "Confidence: " + nf(results[0].confidence, 0,2);
-  text(label, 10, 360);
-  text(confidence, 10, 380);
-  console.log(label);
+    stroke(0);
+    textSize(18);
+    label = "Label: " + results[0].label;
+    confidence = "Confidence: " + nf(results[0].confidence, 0, 2);
+    text(label, 10, 360);
+    text(confidence, 10, 380);
+    console.log(label);
 }
-
-
-myDiv = document.getElementById('myDiv');
-console.log("This is my Div:" + myDiv);
-Plotly.newPlot( myDiv, [{
-	x: [1, 2, 3, 4, 5],
-	y: [1, 2, 4, 8, 16] }], {
-	margin: { t: 0 } } );
 
 //Plotly.newPlot('myDiv', data, layout);
