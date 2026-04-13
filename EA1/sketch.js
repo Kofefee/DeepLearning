e648a865444258e75982cd2f0e191f914595a14b
Felix <felix.koelle@hs-furtwangen.de>
2026-04-13 16:01:10 +0200

    test123

--- EA1/sketch.js
+++ EA1/sketch.js
@@ -1,17 +1,16 @@
 let classifier;
 
 let images = [
-    { src: "images/good1.jpg", label: "", conf: 0 },
-    { src: "images/good2.jpg", label: "", conf: 0 },
-    { src: "images/good3.jpg", label: "", conf: 0 },
-    { src: "images/bad1.jpg", label: "", conf: 0 },
-    { src: "images/bad2.jpg", label: "", conf: 0 },
-    { src: "images/bad3.jpg", label: "", conf: 0 }
+    { src: "images/good1.jpg", label: "", conf: 0, type: "good" },
+    { src: "images/good2.jpg", label: "", conf: 0, type: "good" },
+    { src: "images/good3.jpg", label: "", conf: 0, type: "good" },
+    { src: "images/bad1.jpg", label: "", conf: 0, type: "bad" },
+    { src: "images/bad2.jpg", label: "", conf: 0, type: "bad" },
+    { src: "images/bad3.jpg", label: "", conf: 0, type: "bad" }
 ];
 
 let loadedImages = [];
-
-let canvasText = 'Drag an image file onto the canvas.';
+let canvas;
 
 function preload() {
     classifier = ml5.imageClassifier("MobileNet");
@@ -22,25 +21,18 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(400, 400);
+    canvas = createCanvas(900, 700);
+    canvas.drop(gotFile);
 
-    // Klassifiziere alle Bilder
+    // Klassifizieren
     for (let i = 0; i < loadedImages.length; i++) {
         classifyImage(i);
     }
-
-    // Drag and Drop
-    let dropArea = createCanvas(710, 400);
-    dropArea.drop(gotFile);
-    //noLoop();
 }
 
 function classifyImage(index) {
     classifier.classify(loadedImages[index], (err, results) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
+        if (err) return;
 
         images[index].label = results[0].label;
         images[index].conf = results[0].confidence;
@@ -52,18 +44,29 @@ function classifyImage(index) {
 function draw() {
     background(240);
 
+    textSize(16);
+    fill(0);
+
+    text("Gute Beispiele", 50, 30);
+    text("Schlechte Beispiele", 50, 360);
+
     for (let i = 0; i < loadedImages.length; i++) {
 
-        let y = i * 100;
+        let isGood = images[i].type === "good";
+
+        let row = isGood ? i : i - 3;
+        let yOffset = isGood ? 50 : 380;
+
+        let y = yOffset + row * 100;
 
         // Bild links
-        image(loadedImages[i], 10, y, 80, 80);
+        image(loadedImages[i], 20, y, 80, 80);
 
-        // Text
+        // Text daneben
         fill(0);
         textSize(12);
-        text(images[i].label, 100, y + 30);
-        text(nf(images[i].conf * 100, 2, 2) + "%", 100, y + 50);
+        text(images[i].label, 120, y + 30);
+        text(nf(images[i].conf * 100, 2, 2) + "%", 120, y + 50);
     }
 }
 
@@ -79,22 +82,28 @@ function drawPieChart(index) {
 
     let layout = {
         height: 100,
-        width: 150
+        width: 150,
+        margin: { t: 10, b: 10, l: 10, r: 10 }
     };
+
+    let isGood = images[index].type === "good";
+    let row = isGood ? index : index - 3;
+    let yOffset = isGood ? 50 : 380;
+
+    let y = yOffset + row * 100;
 
     let divId = "chart" + index;
 
     if (!document.getElementById(divId)) {
         let div = createDiv().id(divId);
-        div.position(300, index * 100);
+        div.position(350, y);
     }
 
     Plotly.newPlot(divId, data, layout);
 }
 
-// Upload Funktion
+// 📤 Upload Funktion
 function gotFile(file) {
-
     if (file.type === 'image') {
 
         let img = createImg(file.data, '').hide();
@@ -105,9 +114,12 @@ function gotFile(file) {
             let label = results[0].label;
             let conf = results[0].confidence;
 
-            // Anzeige
-            let div = createDiv("Upload Ergebnis: " + label + " (" + nf(conf * 100, 2, 2) + "%)");
-            div.position(10, 620);
+            let y = 650;
+
+            let resultText = createDiv(
+                "Upload: " + label + " (" + nf(conf * 100, 2, 2) + "%)"
+            );
+            resultText.position(20, y);
 
             let data = [{
                 values: [conf, 1 - conf],
@@ -115,8 +127,13 @@ function gotFile(file) {
                 type: 'pie'
             }];
 
-            Plotly.newPlot(div.elt, data);
-        });
+            let div = createDiv();
+            div.position(350, y);
 
+            Plotly.newPlot(div.elt, data, {
+                height: 120,
+                width: 180
+            });
+        });
     }
 }
