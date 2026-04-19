dc9b6e6f06741824ccaa23115c3d0fcb56422df9
Felix <felix.koelle@hs-furtwangen.de>
2026-04-19 14:27:22 +0200

    Testing marial with script header

--- EA1/sketch.js
+++ EA1/sketch.js
@@ -1,8 +1,3 @@
-// index.js
-import '@material/web/button/filled-button.js';
-import '@material/web/button/outlined-button.js';
-import '@material/web/checkbox/checkbox.js';
-
 let classifier;
 let img;
 
