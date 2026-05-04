a6499b1ec2d9573b544eb85e896bee2e9c23272e
Felix <felix.koelle@hs-furtwangen.de>
2026-05-04 20:52:07 +0200

    else if to if

--- EA1/sketch.js
+++ EA1/sketch.js
@@ -123,11 +123,14 @@ function createResultRow(imageSrc, results, divName) {
     let values = results.map(r => r.confidence);
     let data = [{ values, labels, type: 'pie' }];
 
+    console.log(imageSrc);
     let colorway = ['#1a1a1a', '#555', '#888', '#aaa', '#ccc', '#e0e0e0'];
     if (imageSrc.includes("good")) {
         colorway = ['#2d6a4f', '#52b788', '#95d5b2', '#b7e4c7', '#d8f3dc', '#f0faf3'];
-    } else if (imageSrc.includes("bad")) {
+    } 
+    if (imageSrc.includes("bad")) {
         colorway = ['#7d0000', '#c1121f', '#e63946', '#f4a261', '#f8c8a0', '#fde8d8'];
+        console.log("This is so bad!");
     }
 
     let localLayout = {
